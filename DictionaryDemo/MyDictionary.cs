1c2217420fb1ba2520b0998c8dffd68df53918b6
Yiğit Arpacıoğlu <yigit.arpacioglu@gmail.com>
2021-01-24 01:04:40 +0300

    Add project files.

---
+++ DictionaryDemo/MyDictionary.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+namespace DictionaryDemo
+{
+    class MyDictionary<TKey, TValue>
+    {
+        private TKey[] keys;
+        private TValue[] values;
+
+        public MyDictionary()
+        {
+            keys = new TKey[0];
+            values = new TValue[0];
+        }
+
+        public void Add(TKey key, TValue value)
+        {
+            if (keys.Contains(key) || values.Contains(value))
+            {
+                Console.WriteLine("Overlapping key or value, please change the values");
+            }
+                
+            var tempKeyArray = keys;
+            var tempValueArray = values;
+
+            keys = new TKey[keys.Length + 1];
+            values = new TValue[values.Length + 1];
+
+            for (int i = 0; i < keys.Length - 1; i++)
+            {
+                keys[i] = tempKeyArray[i];
+                values[i] = tempValueArray[i];
+            }
+
+            keys[keys.Length - 1] = key;
+            values[values.Length - 1] = value;
+        }
+
+        public void GetAll()
+        {
+            for (int i = 0; i < keys.Length; i++)
+            {
+                Console.WriteLine("Key: {0} -- Value: {1}", keys[i], values[i]);
+            }
+            
+        }
+        public void GetByKey(TKey key)
+        {
+            Console.WriteLine("Key: {0} -- Value: {1}",keys[Array.IndexOf(keys,key)],values[Array.IndexOf(keys, key)]);
+        }
+    }
+}
---
+++ DictionaryDemo/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DictionaryDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> dictionary = new MyDictionary<int, string>();
+            dictionary.Add(1,"Yiğit");
+            dictionary.Add(2,"Cihat");
+            dictionary.Add(3,"Berkay");
+
+            dictionary.GetAll();
+            dictionary.GetByKey(2);
+        }
+    }
+}
